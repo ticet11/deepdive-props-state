28bde56e452857d545d488c1ae8cbee2cfc4272d
Brian Kozub <ticet11@gmail.com>
2020-05-14 11:21:16 -0600

    Initial Commit.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>React, Props, and State</h1>
+            </div>
+        );
+    }
+}
